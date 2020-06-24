6d08e83e5867c014b3116e7b04e2ed73a062d1b6
Mik3y-F <mike18farad@gmail.com>
2020-06-24 13:33:23 +0300

    Refactored User Model

--- lipo/users/models.py
+++ lipo/users/models.py
@@ -3,21 +3,31 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.urls import reverse
 from django.utils.translation import ugettext_lazy as _
+from django.utils.text import slugify
 
 
 class User(AbstractUser):
 
     # First Name and Last Name do not cover name patterns
     # around the globe.
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    id = models.UUIDField(_("Review Id"), primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(_("Name of User"), blank=True, max_length=255)
     first_name = models.CharField(_("First Name of User"), blank=True, max_length=255)
     last_name = models.CharField(_("Last Name of User"), blank=True, max_length=255)
     user_type = models.ManyToManyField("users.UserType", verbose_name=_("user types"),
-                                       default=UserType.NORMAL_USER,)
+                                       default="users.UserType.NORMAL_USER",)
 
     def get_absolute_url(self):
         return reverse("users:detail", kwargs={"username": self.username})
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+    def get_full_name(self):
+        return f'{self.first_name} {self.last_name}'
+
+    def get_full_name_slug(self):
+        return slugify(f'{self.first_name} {self.last_name}')
 
 
 class UserType(models.Model):
@@ -37,11 +47,11 @@ class UserType(models.Model):
     )
 
     class Meta:
-        verbose_name = _("UserType")
-        verbose_name_plural = _("UserTypes")
+        verbose_name = _("User Type")
+        verbose_name_plural = _("User Types")
 
     def __str__(self):
         return self.name
 
     def get_absolute_url(self):
-        return reverse("UserType_detail", kwargs={"pk": self.pk})
+        return reverse("users_types:detail", kwargs={"pk": self.pk})
