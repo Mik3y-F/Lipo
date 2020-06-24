f97aaaf1e9fdf233edcf7d33e52d3a91618c5d1a
Mik3y-F <mike18farad@gmail.com>
2020-06-24 13:29:27 +0300

    Created votes app and added vote models

---
+++ lipo/votes/models.py
@@ -0,0 +1,49 @@
+import uuid
+from django.db import models
+
+
+class PsvReviewVote(models.Model):
+
+    id = models.UUIDField(_("Review Vote Id"), primary_key=True, default=uuid.uuid4,
+                          editable=False)
+    author = models.ForeignKey("users.User", verbose_name=_("Review Vote Author"),
+                               on_delete=models.CASCADE)
+    review = models.ForeignKey("reviews.PsvReview", verbose_name=_("Review being voted for"),
+                               on_delete=models.CASCADE)
+    type = models.ForeignKey("votes.VoteType", verbose_name=_("Type of Review Vote"),
+                             on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = _("Review Vote")
+        verbose_name_plural = _("Review Votes")
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("review_vote:detail", kwargs={"pk": self.pk})
+
+
+class VoteType(models.Model):
+
+    DOWN_VOTE = 'down'
+    UP_VOTE = 'up'
+    VOTE_TYPE = [
+        (DOWN_VOTE, _('Vote is a down vote')),
+        (UP_VOTE, _('Vote is an up vote')),
+    ]
+
+    name = models.CharField(
+        max_length=32,
+        choices=VOTE_TYPE,
+    )
+
+    class Meta:
+        verbose_name = _("VoteType")
+        verbose_name_plural = _("VoteTypes")
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("vote_type:detail", kwargs={"pk": self.pk})
